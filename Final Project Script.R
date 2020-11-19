2519057c126c80e0c04a36ad29edb9baeaaad166
Bryan Lu <Byl222@lehigh.edu>
2020-11-18 19:44:08 -0500

    edited final project

--- Final Project Script.R
+++ Final Project Script.R
@@ -1,9 +1,22 @@
 library(tidyverse)
+library(dplyr)
+library(gapminder)
+library(ggrepel)
+library(socviz)
+library(ggridges)
 
-temp = read_csv("net-price.csv")
-templ <- as.matrix(temp)
+#average tuition for public 4 year instate 
 
+temp = read_csv("Private tuition.csv")
 view(temp)
- 
- #Test 
- 
+
+p <- ggplot(data = temp,
+            mapping = aes(x = year,
+                          y = public_4yr_instate))+
+  geom_line(data = temp,
+            mapping = aes(x = year,
+                          y = private ))
+p + geom_line()+
+  labs(y="Private and Public tuition in dollars",
+    title= "College tuition changes")
+
